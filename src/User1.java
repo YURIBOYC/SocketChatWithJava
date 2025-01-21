490875e85396c29718c56dcda666ce80e0adb576
ABISHEK <abideepu19.vrs@gmail.com>
2025-01-21 21:56:39 +0530

    Final Commit

--- src/User1.java
+++ src/User1.java
@@ -1,30 +1,79 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-public class User1 extends Frame implements ActionListener {
+public class User1 extends Frame implements Runnable, ActionListener {
     TextField textField;
     TextArea textArea;
     Button send;
-    User1(){
-        textField = new TextField();
-        textArea = new TextArea();
+
+    ServerSocket serverSocket;
+    Socket socket;
+    DataInputStream dataInputStream;
+    DataOutputStream dataOutputStream;
+
+    Thread chat;
+
+    User1() {
+        textField = new TextField(40);
+        textArea = new TextArea(20, 50);
         send = new Button("Send message");
         send.addActionListener(this);
+
         add(textField);
         add(send);
         add(textArea);
-        setSize(500,700);
+
         setLayout(new FlowLayout());
+        setSize(600, 400);
         setVisible(true);
+
+        try {
+            serverSocket = new ServerSocket(12000);
+            textArea.append("Waiting for connection...\n");
+            socket = serverSocket.accept();
+            textArea.append("Connected to a client.\n");
+            dataInputStream = new DataInputStream(socket.getInputStream());
+            dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+            chat = new Thread(this);
+            chat.start();
+        } catch (IOException e) {
+            System.out.println(e);;
+        }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        String msg = textField.getText();
+        textArea.append("Abishek: " + msg + "\n");
+        textField.setText("");
+        try {
+            dataOutputStream.writeUTF(msg);
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+    }
 
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                String msg = dataInputStream.readUTF();
+                textArea.append("Deephak: " + msg + "\n");
+            } catch (IOException e) {
+                System.out.println(e);;
+                break;
+            }
+        }
     }
 
     public static void main(String[] args) {
-
+        new User1();
     }
 }
--- src/User2.java
+++ src/User2.java
@@ -1,30 +1,75 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 
-public class User2 extends Frame implements ActionListener {
+public class User2 extends Frame implements Runnable, ActionListener {
     TextField textField;
     TextArea textArea;
     Button send;
-    User2(){
-        textField = new TextField();
-        textArea = new TextArea();
+
+    Socket socket;
+    DataInputStream dataInputStream;
+    DataOutputStream dataOutputStream;
+
+    Thread chat;
+
+    User2() {
+        textField = new TextField(40);
+        textArea = new TextArea(20, 50);
         send = new Button("Send message");
         send.addActionListener(this);
+
         add(textField);
         add(send);
         add(textArea);
-        setSize(500,700);
+
         setLayout(new FlowLayout());
+        setSize(600, 400);
         setVisible(true);
+
+        try {
+            socket = new Socket("localhost", 12000);
+            textArea.append("Connected to the server.\n");
+            dataInputStream = new DataInputStream(socket.getInputStream());
+            dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+            chat = new Thread(this);
+            chat.start();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        String msg = textField.getText();
+        textArea.append("Deephak: " + msg + "\n");
+        textField.setText("");
+        try {
+            dataOutputStream.writeUTF(msg);
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+    }
 
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                String msg = dataInputStream.readUTF();
+                textArea.append("Abishek: " + msg + "\n");
+            } catch (IOException e) {
+                System.out.println(e);
+                break;
+            }
+        }
     }
 
     public static void main(String[] args) {
-
+        new User2();
     }
 }
